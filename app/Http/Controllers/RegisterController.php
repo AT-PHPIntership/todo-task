14427a545ef20da4ea0c8b9c67c771c5c509fa2d
LTT0013 <manh.le@asiantech.vn>
2018-08-22 11:56:39 +0700

    build basic layout for parent layout and login/register layout

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,11 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-use App\Login;
 
-class LoginController extends Controller
+class RegisterController extends Controller
 {
   public function index() {
-    return view('auths.login');
+    return view('auths.register');
   }
 }
