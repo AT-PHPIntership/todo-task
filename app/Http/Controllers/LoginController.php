a63a2e3cd4bf82a64a29cf078db3d5cef9d209d3
LTT0013 <manh.le@asiantech.vn>
2018-08-22 14:55:22 +0700

    complete login form

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Validator;
 use App\Http\Requests;
 use App\Login;
 
@@ -11,5 +11,18 @@ class LoginController extends Controller
 {
   public function index() {
     return view('auths.login');
+  }
+  public function login(Request $request) 
+  {
+    $validator = Validator::make($request->all(), [
+      'username' => 'required',
+      'password' => 'required|min:8',
+    ]);
+      if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+      }
+    
   }
 }
--- resources/views/auths/login.blade.php
+++ resources/views/auths/login.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-  <div>Login</div>
+  <section class="login-section">
+    @include('common.errors')
+    <form action="/login" method="post" class="form-horizontal col-md-4 m-auto">
+    <input name="_token" type="hidden" id="_token" value="{{ csrf_token() }}" />
+      <div class="form-group">
+        <label for="username" class="control-label">Username: </label>
+        <input type="text" name="username" id="username" class="form-control">
+      </div>
+      <div class="form-group">
+        <label for="password" class="control-label">Password: </label>
+        <input type="password" name="password" id="password" class="form-control">
+      </div>
+      <a href="/register" class="d-block pb-2 ">Create new account</a>
+      <button type="submit" class="btn btn-primary col-md-12">Login</button>
+    </form>
+  </section>
 @endsection
